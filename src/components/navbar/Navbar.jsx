0ded26f0abc3997f2939b96ca5022c6299c7b997
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-05-09 09:12:28 +0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="logo">
+            
+        </div>
+        <div className="links"></div>
+    </div>
+  )
+}
+
+export default Navbar
