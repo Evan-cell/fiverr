4f5841271c92be479df54ce1f01e838279bee5a9
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-06-05 14:16:44 +0300

    trusties section

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Featured from '../../featured/Featured'
+import Trustedby from '../../Trusted/Trustedby'
 
 function Home() {
   return (
     <div>
       <Featured />
+      <Trustedby />
     </div>
   )
 }
