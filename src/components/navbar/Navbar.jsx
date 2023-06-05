0805024a449f60b98a43184762c7a562a88f599d
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-06-05 08:51:28 +0300

    active state

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Navbar.css'
+import React, {useState} from 'react'
+import './Navbar.scss'
+
 function Navbar() {
+  const [active,setActive] = useState(false)
   return (
-    <div className='navbar'>
+    <div className={active ? "navbar active": "navbar"}>
+        <div className="container">
         <div className="logo">
-            
+            <span className="text">fiverr</span>
+            <span className="dot">.</span>
         </div>
-        <div className="links"></div>
+        <div className="links">
+            <span>Fiver Business</span>
+            <span>Explore</span>
+            <span>English</span>
+            <span>Sign In</span>
+            <span>Become a Seller</span>
+            <button>Join</button>
+        </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Test1</span>
+          <span>Test2</span>
+        </div>
     </div>
   )
 }
