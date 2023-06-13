dc3efe71ced34773d8deec324369cfcfb8627512
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-06-13 11:33:57 +0300

    gigcard

--- src/components/gigCard/Gigcard.jsx
+++ src/components/gigCard/Gigcard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
-function Gigcard() {
+import  './Gigcard.scss'
+import { Link } from 'react-router-dom'
+function Gigcard({item}) {
   return (
-    <div>Gigcard</div>
+    <Link to='/gig/123'>
+    <div className='gigcard'>
+        <img src={item.img} alt="" />
+        <div className="info">
+            <div className="user">
+                <img src={item.PP} alt="" />
+                <span>{item.username}</span>
+            </div>
+            <p>{item.desc}</p>
+            <div className="star">
+                <img src="./img/star.png" alt="" />
+                <span>{item.star}</span>
+            </div>
+        </div>
+        <hr />
+        <div className="details">
+            <img src="./img/heart.png" alt="" />
+            <div className="price">
+            <span>STARTING AT</span>
+            <h2>${item.price}</h2>
+            </div>
+
+        </div>
+    </div>
+    </Link>
+    
   )
 }
 
